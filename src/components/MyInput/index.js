5fd8b34b37cbbd3aaa61f4e4890a8aa90545dc82
Valentin Brosseau <c4software@gmail.com>
2017-05-06 21:21:21 +0200

    Switch to folder structure for compontents

--- src/components/MyInput/index.js
+++ src/components/MyInput/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./index.css"
 
 class MyInput extends Component{
   handleChange(e){
--- src/components/Titre/index.js
+++ src/components/Titre/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./index.css"
 
 class Titre extends Component{
   constructor(props){
