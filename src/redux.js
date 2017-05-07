fb5ce06a01de050c69719a04fc1b148ad746a84c
Valentin Brosseau <c4software@gmail.com>
2017-05-07 11:40:32 +0200

    Set default state

--- src/redux.js
+++ src/redux.js
@@ -1,5 +1,7 @@
 import { createStore } from 'redux'
 
+let DEFAULT_STAT = [];
+
 function list(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_LIST':
@@ -9,6 +11,6 @@ function list(state = [], action) {
   }
 }
 
-let store = createStore(list, [])
+let store = createStore(list, DEFAULT_STAT)
 
 export default store;
