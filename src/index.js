832c59fabb56ff9239da2369e4e01e29a9d25dd1
Valentin Brosseau <c4software@gmail.com>
2017-05-07 12:22:11 +0200

    Rewrite to import and use Route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
 import { Provider } from 'react-redux';
 import store from './redux';
+
+import routes from './routes';
 
 store.dispatch({
   type: 'ADD_TO_LIST',
@@ -13,7 +13,7 @@ store.dispatch({
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    {routes}
   </Provider>,
   document.getElementById('root')
 );
