6c49fa70c4ad3f958195aa6226a3de219a75bb4f
Valentin Brosseau <c4software@gmail.com>
2017-05-07 12:19:22 +0200

    Add Minimalist Routing

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  HashRouter as Router,
+  Route,
+	Link
+} from 'react-router-dom'
+
+import Home from './views/home';
+import List from './views/list';
+
+
+// Route de l’application
+const routes = (
+	<Router>
+		<div>
+				<div className="text-center pad10">
+					<Link to="/">Home</Link> | <Link to="/list">List</Link>
+				</div>
+				<Route exact path="/" component={Home} />
+				<Route path="/list" component={List} />
+		</div>
+	</Router>
+);
+
+export default routes;
