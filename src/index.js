df41d4d24a47162ab9d9a56d5f70debeaa6ebc7b
Valentin Brosseau <c4software@gmail.com>
2017-05-11 21:30:38 +0200

    Change code to use the function

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import store from './redux';
 
 import routes from './routes';
 
-store.dispatch({
-  type: 'ADD_TO_LIST',
-  text: "Example data"
-})
+import store from "./redux"
+import {add_to_list} from "./actions";
+
+add_to_list("Example data")
 
 ReactDOM.render(
   <Provider store={store}>
